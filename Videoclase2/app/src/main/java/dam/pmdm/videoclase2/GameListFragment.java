7bf5019cbeef730ede376113b83318ce8c11db39
Blackmoon1812 <93385348+Blackmoon1812@users.noreply.github.com>
2024-12-14 20:46:19 +0100

    Commit Inicial de la carpeta PMDM, incluye 2 proyectos de las clases online, el primer proyecto está completo, el segundo está casi terminado

---
+++ Videoclase2/app/src/main/java/dam/pmdm/videoclase2/GameListFragment.java
@@ -0,0 +1,101 @@
+package dam.pmdm.videoclase2;
+
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import dam.pmdm.videoclase2.databinding.GameListFragmentBinding;
+
+public class GameListFragment  extends Fragment {
+
+    private GameListFragmentBinding binding; //Binding para el layout
+    private ArrayList<GameData> games; //Lista de juegos
+    private GameRecyclerViewAdapter adapter; //Adaptador del RecyclerView
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        //Inflar el layout utilizando el binding
+        binding = GameListFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated)@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        // Inicializa la lista de juegos
+        loadGames(); //Cargar los juegos (puedes implementar esta función para obtener datos)
+
+        // Configurar el RecyclerView
+        adapter = new GameRecyclerViewAdapter (games, getActivity());
+        binding.gamesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.gamesRecyclerView.setAdapter(adapter);
+
+    }
+
+    //Método para cargar juegos (puedes implementar tu lógica aquí)
+    private void loadGames() {
+        games = new ArrayList<GameData>();
+        //Llenar la lista con datos de videojuegos
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/71uS8Ra1aGL._AC_uf894,1000_QL80_.jpg", //Reemplaza con la ruta de la imagen
+                "Super Mario Bros. Wonder",
+                2023,
+                "Un juego de plataformas en un mundo vibrante lleno de sorpresas.",
+                "Nintendo Switch"
+
+        ));
+
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/71C9pOGlKtL.jpg", //Reemplaza con la ruta de la imagen
+                "Mario Kart 8 Deluxe",
+                2017,
+                "Una versión mejorada de Mario Kart 8 con un modo de batalla renovado.",
+                "Nintendo Switch"
+
+        ));
+
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/81CbYA2Gt3L._AC_UF894,1000_QL80_.jpg", //Reemplaza con la ruta de la imagen
+                "Super Mario 3D World + Browser's Fury",
+                2021,
+                "Aventura cooperativa en 3D con una misión en el mundo abierto de Browser's Fury.",
+                "Nintendo Switch"
+
+        ));
+
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/81d3KS1-60L.jpg", //Reemplaza con la ruta de la imagen
+                "Super Mario Maker 2",
+                2019,
+                "Diseña y comparte tus propios niveles de Mario en esta secuela.",
+                "Nintendo Switch"
+
+        ));
+
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/81drkVN7GRL._AC_UF1000,1000_QL80_.jpg", //Reemplaza con la ruta de la imagen
+                "Super Mario Odissey",
+                2017,
+                "Mario debe rescatar a la Princesa Peach utilizando a su compañero, Cappy.",
+                "Nintendo Switch"
+
+        ));
+
+        games.add(new GameData(
+                "https://m.media-amazon.com/images/I/71FxEVBYddL._AC_UF1000,1000_QL80_.jpg", //Reemplaza con la ruta de la imagen
+                "Super Mario All-Stars",
+                2021,
+                "Colección remasterizada de los clásicos juegos de Mario.",
+                "Nintendo Switch"
+
+        ));
+    }
+}
---
+++ Videoclase2/app/src/main/java/dam/pmdm/videoclase2/MainActivity.java
@@ -0,0 +1,54 @@
+package dam.pmdm.videoclase2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import dam.pmdm.videoclase2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Configura el NavController
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    //Metodo para manejar el clic en un juego
+    public void gameClicked(GameData game, View view) {
+        //Crear un Bundle para pasar los datos al GameDetailFragment
+        Bundle bundle = new Bundle();
+        bundle.putString("name", game.getName()); //Pasa el nombre del juego
+        bundle.putString("releaseYear", String.valueOf(game.getReleaseYear()));
+        bundle.putString("platforms", String.valueOf(game.getPlatforms()));
+        bundle.putString("image", game.getImage()); // Pasa la imagen del juego
+        bundle.putString("description", game.getDescription()); // Pasa la descripción o más datos que necesites
+
+        //Navegar al GameDetailFragment con el Bundle
+        Navigation.findNavController(view).navigate(R.id.gameDetailFragment, bundle);
+    }
+
+    @Override
+    public boolean onSupportNavigationUp() {
+        //Utiliza el método navigateUp del NavController
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+}
